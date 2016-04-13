4c67c96fec5ef6c4f651876e3e92ecf08bcd48e2
taibaiyifeng <taibaiyifeng@outlook.com>
2016-04-13 17:39:33 +0800

    使用php artisan make:seeder PostTableSeeder命令创建PostTableSeeder,之后对文件进行编辑,再执行php artisan db:seed命令

---
+++ database/seeds/PostTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      
+        	App\Post::truncate();
+        	factory(App\Post::class,20)->create();
+        
+    }
+}
