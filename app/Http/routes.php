eb85511bc1d01ab7d2aa0139eaf21ea8d1b9b313
taibaiyifeng <taibaiyifeng@outlook.com>
2016-04-13 20:15:12 +0800

    添加后台路由

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,19 @@ Route::get('/', function () {
 });
 Route::get('blog','BlogController@index');
 Route::get('blog/{slug}','BlogController@showPost');
+
+
+//Admin area
+Route::get('admin',function(){
+	return redirect('/admin/post');
+});
+Route::group(['namespace' => 'Admin','middleware' => 'auth'],function(){
+	resource('admin/post','PostController');
+	resource('admin/tag','TagController');
+	get('admin/upload','UploadController@index');
+});
+
+// Logging in  and out
+Route::get('/auth/login','Auth\AuthController@getLogin');
+Route::post('/auth/login','Auth\AuthController@postLogin');
+Route::get('/auth/logout','Auth\AuthController@getLogout')
