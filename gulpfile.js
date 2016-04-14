7835b19dd5b217909abe4082e32e0e16bea10bbd
taibaiyifeng <taibaiyifeng@outlook.com>
2016-04-14 10:36:45 +0800

    对gulp依赖资源的更改

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
 var elixir = require('laravel-elixir');
 
 /*
@@ -10,7 +12,47 @@ var elixir = require('laravel-elixir');
  | file for our application, as well as publishing vendor resources.
  |
  */
+gulp.task("copyfiles",function(){
+	gulp.src("vendor/bower_dl/jquery/dist/jquery.js")
+		.pipe(gulp.dest("resources/assets/js/"));
+	gulp.src("vendor/bower_dl/bootstrap/less/**")
+		.pipe(gulp.dest("resources/assets/less/bootstrap"));
+	gulp.src("vendor/bower_dl/bootstrap/dist/js/bootstrap.js")
+		.pipe(gulp.dest("resources/assets/js/"));
+	gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
+		.pipe(gulp.dest("public/assets/fonts"));
+	gulp.src("vendor/bower_dl/font-awesome/less/**")
+        		.pipe(gulp.dest("resources/assets/less/fontawesome"));
 
+   	 gulp.src("vendor/bower_dl/font-awesome/fonts/**")
+       	 	.pipe(gulp.dest("public/assets/fonts"));
+
+    	// 拷贝 datatables
+   	 var dtDir = 'vendor/bower_dl/datatables-plugins/integration/';
+
+   	 gulp.src("vendor/bower_dl/datatables/media/js/jquery.dataTables.js")
+        		.pipe(gulp.dest('resources/assets/js/'));
+
+    	gulp.src(dtDir + 'bootstrap/3/dataTables.bootstrap.css')
+        		.pipe(rename('dataTables.bootstrap.less'))
+        		.pipe(gulp.dest('resources/assets/less/others/'));
+
+    	gulp.src(dtDir + 'bootstrap/3/dataTables.bootstrap.js')
+        		.pipe(gulp.dest('resources/assets/js/'));
+
+
+});
 elixir(function(mix) {
-    mix.less('app.less');
+	//combain scripts
+	mix.scripts([
+		'js/jquery.js',
+		'js/bootstrap.js',
+		'js/jquery.dataTables.js',
+		'js/dataTables.bootstrap.js'
+		],
+		'public/assets/js/admin.js',
+		'resources/assets'
+		);
+	//compile Less
+    mix.less('admin.less','public/assets/css/admin.css');
 });
